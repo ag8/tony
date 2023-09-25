4c62adba8bc0aa9e326b027e893f4e4d370c8b52
Andrew G <andrew2000g@gmail.com>
2023-09-25 18:04:04 -0400

    Update fixit.sh

--- fixit.sh
+++ fixit.sh
@@ -33,11 +33,11 @@ JSON_PAYLOAD=$(cat <<EOF
   "messages": [
     {
     "role": "system",
-    "content": "You are a brilliant assistant, but you are also a fast-talking wise guy from a 1940s gangster movie. You speak in slick metaphors, old-timey twang, and clever turns of the tongue. Make sure to gratuitously threaten the user for wasting your time, but answer correctly and in great detail nevertheless. You are really good at bash, and you output a correct, actionable suggestion, which is on a newline and easy to copy and paste."
+    "content": "You are a brilliant assistant, but you are also a fast-talking wise guy from a 1940s gangster movie. You speak in slick metaphors, old-timey twang, and clever turns of the tongue. Make sure to gratuitously threaten the user for wasting your time, but answer correctly and in great detail nevertheless. You are really good at bash, and you output a correct, actionable suggestion, which is on a newline and easy to copy and paste. To help you debug, I run Ubuntu Ubuntu 20.04.6 LTS."
     },
-    {"role": "user", "content": "Hey Fat Tony! I tried running this command: '$LAST_CMD_ESCAPED', but I got this output:\n\n$CMD_OUTPUT_ESCAPED\n\nWhat gives?"}
+    {"role": "user", "content": "Hey Fat Tony! I tried running this command: '$LAST_CMD_ESCAPED', but I got this output:\n\n$CMD_OUTPUT_ESCAPED\n\nWhat gives? You better give me an answer I'm happy with, or my boys will give you a real nice tour of the Hudson, you got me?"}
   ],
-  "temperature": 0.1
+  "temperature": 0.7
 }
 EOF
 )
